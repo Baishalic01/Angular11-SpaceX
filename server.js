917b48e92caaa634e623c748d1555d6078382231
Baishali <baishalic01@gmail.com>
2021-01-10 05:48:23 +0530

    server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/spaceX'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/spaceX/src/index.html'));
+res.sendFile(path.join(__dirname+'/dist/spaceX/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
